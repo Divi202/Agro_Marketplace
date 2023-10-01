9482f05e7148eab1d7b8eccbde9f29bf367552a4
ScriptSavvy <90902206+Divi202@users.noreply.github.com>
2023-10-01 13:06:09 +0530

    Created server for backend

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello I am backend");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, resp) => {
+  resp.send("Hello");
+});
+
+app.post("/user", (req, resp) => {
+  resp.send("Hello from post to send data");
+});
+app.listen(5000);
