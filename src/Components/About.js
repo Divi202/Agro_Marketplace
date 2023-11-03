a9b7376c3c5a179832e542c783c0a6e1986942df
ScriptSavvy <90902206+Divi202@users.noreply.github.com>
2023-11-03 13:45:50 +0530

    solved the image problem in about Component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,17 @@
-
 import AboutDiv from "./About-div/About-Div";
 import "../style/About.css";
-import Photo from "3rd Sem MCA Project/Agro_Marketplac/src/images/bg-product-1.png";
+import Photo from "src/images/bg-product-1.png";
+import Image from "next/image";
 export default function About() {
   return (
     <div className="about-main-div">
       <div className="left-div">
-        <img 
-        src = {Photo}
-        ></img>
+        <Image
+          src={Photo}
+          style={{
+            height: "100%",
+          }}
+        ></Image>
       </div>
       <div className="right-div">
         <h4 style={{ color: "Green" }}> About-us</h4>
